3d9dc636eec9d8850b36adb4ee0e95550eec1ced
onDeivid <141053918+OnDeivid@users.noreply.github.com>
2024-08-19 04:18:04 +0300

    add shadow png

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -1,33 +1,44 @@
-import React from 'react'
-import twitch from '../../assets/darkTwitch.png'
-import idkp2p from '../../assets/idkp2p1.png'
+import twitch from '../../assets/dTwitch.png'
+import marvelDC from '../../assets/marvelDC.png'
+import onlyfans from '../../assets/onlyfans.png'
+import shadow from '../../assets/shadow6.png'
+
 import './Home.css'
 export default function Home() {
     return (
         <main>
 
-            <h1 style={{ position: 'absolute', top: 10 }}>its u r choice </h1>
+            <h1 style={{ position: 'absolute', top: 50 }}>its u r choice </h1>
             <div className='container'>
                 <div className='catalog'>
 
-                    <div className='topic-1 twitch'>
-                        <div className='imageHolder'>
-                            <img className='imgOption-1' src={twitch} />
+                    <div className='option-1 twitch'>
+                        <div className='logos'>
+                            <img className='O-twitch' src={twitch} />
+                            <img className='shadow' src={shadow} />
                         </div>
                     </div>
 
-                    <div className='topic-2 marvelDC'>
-                    <div className='imageHolder'>
-                            <img className='imgOption-2' src={idkp2p} />
+                    <div className='option-2 marvelDC'>
+                        <div className='logos'>
+                            <img className='O-marvelDC' src={marvelDC} />
+                            <img className='shadow' src={shadow} />
+
                         </div>
                     </div>
 
-                    <div className='topic-3'>
+                    <div className='option-3 onlyfans' >
+                        <div className='logos'>
+                            <img className='O-onlyfans' src={onlyfans} />
+                            <img className='shadow' src={shadow} />
+                        </div>
                     </div>
 
                 </div>
-            </div>
+                <div style={{ background: 'yellow', width: '80%', height: '70px', position: 'absolute', bottom: '0px' }}></div>
+                <div style={{ background: 'yellow', width: '80%', height: '70px', position: 'absolute', top: '0px' }}></div>
 
+            </div>
         </main>
     )
 }
