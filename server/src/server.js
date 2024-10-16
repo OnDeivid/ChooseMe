a11ee2869adf76f3dc54547655da39c529db799d
onDeivid <141053918+OnDeivid@users.noreply.github.com>
2024-10-16 21:47:25 +0300

    fix cors ORIGIN endpoint

--- server/src/server.js
+++ server/src/server.js
@@ -13,7 +13,7 @@ const app = express();
 
 
 const corsOptions = {
-    origin: 'https://choose-me-deivids-projects-ec29e37b.vercel.app/',
+    origin: 'https://choose-me-deivids-projects-ec29e37b.vercel.app',
     optionsSuccessStatus: 200,
     credentials: false
 }
