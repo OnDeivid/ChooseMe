e5e394e5d7386a1392f7113583fcca587e12d514
onDeivid <141053918+OnDeivid@users.noreply.github.com>
2024-10-20 09:24:27 +0300

    Update main.jsx

--- server/src/server.js
+++ server/src/server.js
@@ -41,6 +41,7 @@ const limiterHeros = rateLimit({
     standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
     legacyHeaders: false, // Disable the `X-RateLimit-*` headers
 });
+ 
 
 app.get('/', async (req, res) => {
     await getAllSectionData()
