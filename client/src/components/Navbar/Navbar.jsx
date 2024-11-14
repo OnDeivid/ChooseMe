27d2e00032376a78bfba5bec9255afffa3fabadc
onDeivid <141053918+OnDeivid@users.noreply.github.com>
2024-11-14 11:11:14 +0200

    Navbar CSS update

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,53 +1,53 @@
-import React, { useState } from 'react'
-import { useNavigate, useParams } from 'react-router-dom'
+import React, { useState } from 'react';
+import { useNavigate, useLocation } from 'react-router-dom';
 
-import './Navbar.css'
+import './Navbar.css';
 
 export default function Navbar() {
-    const [isMenuOpen, setIsMenuOpen] = useState(false);
-
+    const [isMenuOpen, setIsMenuOpen] = useState(true);
     const navigate = useNavigate();
+    const location = useLocation();
 
-    function onEndpoint(endpint) {
-        navigate(endpint)
-    }
+    const toggleMenu = () => setIsMenuOpen(prev => !prev);
+    const handleNavigation = (endpoint) => {
+        navigate(endpoint);
+        setIsMenuOpen(false);
+    };
+
+    const menuItems = [
+        { label: 'Home', endpoint: '/' },
+        { label: 'Heros', endpoint: '/comics' },
+        { label: 'LoL', endpoint: '/lol' },
+        { label: 'CS:GO', endpoint: '/csgo' },
+        { label: 'Cars', endpoint: '/cars' },
+        { label: 'About', endpoint: '/about' }
+    ];
+
+    const MenuItem = ({ label, endpoint }) => (
+        <div
+            className={`endpoint ${location.pathname === endpoint ? 'active' : ''}`}
+            onClick={() => handleNavigation(endpoint)}
+        >
+            <li>{label}</li>
+        </div>
+    );
 
     return (
         <>
-            {!isMenuOpen ?
-                <div className='hiddenMenu' onClick={() => setIsMenuOpen((prev) => !prev)}>
+            {!isMenuOpen ? (
+                <div className='hiddenMenu' onClick={toggleMenu}>
                     <li>Menu</li>
                 </div>
-                :
+            ) : (
                 <div className='navbar'>
-                    <div className={`endpoint`} style={{ borderBottomLeftRadius: 10 }} onClick={() => onEndpoint('/')}>
-                        <li>Home</li>
+                    {menuItems.map((item, index) => (
+                        <MenuItem key={index} label={item.label} endpoint={item.endpoint} />
+                    ))}
+                    <div className='hide' onClick={toggleMenu}>
+                        <li className='closeMenu'>X</li>
                     </div>
-
-                    <div className={`endpoint`} onClick={() => onEndpoint('/comics')}>
-                        <li>Comics</li>
-                    </div>
-
-                    <div className={`endpoint`} onClick={() => onEndpoint('/lol')}>
-                        <li>LoL</li>
-                    </div>
-
-                    <div className={`endpoint`} onClick={() => onEndpoint('/csgo')}>
-                        <li>CS:GO</li>
-                    </div>
-
-                    <div className={`endpoint`} onClick={() => onEndpoint('/cars')}>
-                        <li>Cars</li>
-                    </div>
-
-                    <div className={`endpoint`} onClick={() => onEndpoint('/cars')}>
-                        <li>About</li>
-                    </div>
-
-                    <div className='hide' onClick={() => { setIsMenuOpen(prev => !prev) }}>
-                        <li>X</li>
-                    </div>
-                </div>}
+                </div>
+            )}
         </>
-    )
+    );
 }
