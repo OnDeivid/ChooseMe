02e7c8c77f0ee79b6f986c42d83f854286afdfd6
onDeivid <141053918+OnDeivid@users.noreply.github.com>
2024-11-15 01:07:28 +0200

    Update server.js

--- server/src/server.js
+++ server/src/server.js
@@ -28,26 +28,26 @@ connectDB()
 
 
 // const limiterCars = rateLimit({
-//     windowMs: 900000, 
+//     windowMs: 3600000, 
 //     max: 1, 
 //     standardHeaders: true,
 //     legacyHeaders: false, 
 
 // });
 // const limiterLol = rateLimit({
-//     windowMs: 900000,
+//     windowMs: 3600000,
 //     max: 1, 
 //     standardHeaders: true, 
 //     legacyHeaders: false, 
 // });
 // const limiterHeros = rateLimit({
-//     windowMs: 900000,
+//     windowMs: 3600000,
 //     max: 1, 
 //     standardHeaders: true, 
 //     legacyHeaders: false, 
 // });
 // const getDate = rateLimit({
-//     windowMs: 900000,
+//     windowMs: 3600000,
 //     max: 1, 
 //     standardHeaders: true, 
 //     legacyHeaders: false, 
@@ -77,7 +77,7 @@ app.get('/', async (req, res) => {
 
 })
 
-app.get('/getDate', rateLimit_getDate(1, 900000), (req, res) => {
+app.get('/getDate', rateLimit_getDate(1, 3600000), (req, res) => {
     const currentDate = moment.tz('Europe/Berlin').format('YYYY-MM-DD');
     try {
         res.status(200).json(currentDate);
@@ -110,8 +110,8 @@ app.get('/categorySuggestion/:name', async (req, res) => {
 
 //put request
 
-app.put('/choice/lol/', rateLimit_lol(1, 900000), async (req, res) => {
-    console.log(900000)
+app.put('/choice/lol/', rateLimit_lol(1, 3600000), async (req, res) => {
+    console.log(3600000)
     try {
         const { name } = req.body
         await increaseCount('lol', name)
@@ -122,8 +122,8 @@ app.put('/choice/lol/', rateLimit_lol(1, 900000), async (req, res) => {
     }
 });
 
-app.put('/choice/cars/', rateLimit_cars(1, 900000), async (req, res) => {
-    console.log(900000)
+app.put('/choice/cars/', rateLimit_cars(1, 3600000), async (req, res) => {
+    console.log(3600000)
 
     try {
         const { name } = req.body
@@ -137,8 +137,8 @@ app.put('/choice/cars/', rateLimit_cars(1, 900000), async (req, res) => {
 
 });
 
-app.put('/choice/heros/', rateLimit_heros(1, 900000), async (req, res) => {
-    console.log(900000)
+app.put('/choice/heros/', rateLimit_heros(1, 3600000), async (req, res) => {
+    console.log(3600000)
 
     try {
         const { name } = req.body
@@ -159,4 +159,5 @@ app.put('/donation/lol/', async (req, res) => {
 });
 
 
+app.listen(3000, () => console.log('listening'))
 module.exports = app;
